16c1f1c73fe38a8380c92def5ee7a81ea48b570d
Lisa Antipina <elizavetaaantipina@gmail.com>
2021-08-05 17:18:27 +0300

    added login link test with assertion and exception

--- test_main_page.py
+++ test_main_page.py
@@ -8,3 +8,10 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+def test_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
